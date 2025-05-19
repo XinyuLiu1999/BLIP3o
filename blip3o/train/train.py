9978769f5b2e9c3fa900bde4c8dc8a6083d60b59
JiuhaiChen <jiuhaichen@gmail.com>
2025-05-19 11:30:21 -0400

    Update train.py

--- blip3o/train/train.py
+++ blip3o/train/train.py
@@ -895,7 +895,6 @@ def train(attn_implementation=None):
                 cache_dir=training_args.cache_dir,
                 attn_implementation=attn_implementation,
                 torch_dtype=(torch.bfloat16 if training_args.bf16 else None),
-                use_auth_token="HF_TOKEN_REMOVED",
                 **bnb_model_from_pretrained_args,
             )
     model.config.use_cache = False
